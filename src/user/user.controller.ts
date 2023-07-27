5959262e6c2e49d4cd1c7c8c702b142cbfab45a1
Luiz Gustavo <luizgustavolima200@gmail.com>
2023-07-26 22:12:59 -0500

    teste

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -16,5 +16,6 @@ export class UserController {
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.updatePartial(+id, updateUserDto);
   }
+  //
 
 }
